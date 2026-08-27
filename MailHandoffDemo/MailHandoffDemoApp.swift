a4f38456d46f00d9fb4808fe7d2034685031c3ae
Sadidur Rahman <mdsadidurrahman74@gmail.com>
2026-08-27 16:21:57 +0300

    Initial Commit

---
+++ MailHandoffDemo/MailHandoffDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailHandoffDemoApp.swift
+//  MailHandoffDemo
+//
+//  Created by Sadid on 27.8.2026.
+//
+
+import SwiftUI
+
+@main
+struct MailHandoffDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MailHandoffDemoTests/MailHandoffDemoTests.swift
@@ -0,0 +1,18 @@
+//
+//  MailHandoffDemoTests.swift
+//  MailHandoffDemoTests
+//
+//  Created by Sadid on 27.8.2026.
+//
+
+import Testing
+
+struct MailHandoffDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
